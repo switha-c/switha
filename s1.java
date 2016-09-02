79ab957e221548fd307b9f2be986d173fb57cd4c
switha-c <cswitha@gmail.com>
2016-09-02 12:00:15 +0530

    Create s1.java

---
+++ s1.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+public class s1
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String str=sc.nextLine();
+StringBuffer sb=new StringBuffer(str);
+sb.reverse();
+System.out.println(sb);
+}
+}
