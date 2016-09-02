ee741c2ae90c6028dfe0a757324a5d87caa27a23
switha-c <cswitha@gmail.com>
2016-09-02 10:40:36 +0530

    Create p.java

---
+++ p.java
@@ -0,0 +1,15 @@
+import java.io.*;
+public class p
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+if(n>0)
+System.out.println("positive number");
+else if(n==0)
+System.out.println("zero");
+else
+System.out.println("negative number");
+}
+}
